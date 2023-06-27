300507b16a923398eb709749f23ed1e68bc50e52
algorijun <123007169+HGive@users.noreply.github.com>
2023-06-27 20:48:40 +0900

    first board 수정 1

--- src/main/java/com/sparta/firstboard/service/BoardService.java
+++ src/main/java/com/sparta/firstboard/service/BoardService.java
@@ -5,8 +5,10 @@ import com.sparta.firstboard.dto.BoardResponseDto;
 import com.sparta.firstboard.entity.Board;
 import com.sparta.firstboard.repository.BoardRepository;
 import org.springframework.context.ApplicationContext;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 import java.util.Optional;
@@ -41,13 +43,13 @@ public class BoardService {
     @Transactional
     public Long updateBoard(Long id, BoardRequestDto requestDto) {
         Board board = findBoard(id);
-
-        if(board.getPassword()==requestDto.getPassword()){
+        if(checkPassword(board.getPassword(), requestDto.getPassword())){
             board.update(requestDto);
         }else{
 //            throw new IllegalArgumentException("비밀번호가 일치하지 않습니다.");
-            System.out.println("비밀번호가 일치하지 않습니다.");
-        };
+//            System.out.println("비밀번호가 일치하지 않습니다.");
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "비밀번호가 틀렸습니다.");
+        }
         return id;
     }
 
@@ -57,17 +59,20 @@ public class BoardService {
             boardRepository.delete(board);
         }else{
 //            throw new IllegalArgumentException("비밀번호가 일치하지 않습니다.");
-            System.out.println("비밀번호가 일치하지 않습니다.");
-        };
+//            System.out.println("비밀번호가 일치하지 않습니다.");
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "비밀번호가 틀렸습니다.");
+        }
         return id;
     }
 
 
-
+    private boolean checkPassword(int a, int b){
+        return a==b;
+    }
 
     private Board findBoard(Long id) {
         return boardRepository.findById(id).orElseThrow(() ->
-                new IllegalArgumentException("선택한 게시글은 존재하지 않습니다.")
+                new ResponseStatusException(HttpStatus.BAD_REQUEST, "게시글이 존재하지 않습니다.")
         );
     }
 }
